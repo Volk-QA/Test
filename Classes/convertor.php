f313fc25ed19ea362257347178a8b2367b9df6bc
Volk-QA <56965265+Volk-QA@users.noreply.github.com>
2023-03-05 11:38:15 +0300

    new file:   Classes/convertor.php

---
+++ Classes/convertor.php
@@ -0,0 +1,23 @@
+<?php
+class Convertor
+{
+    private $rate = [
+        "usd" => 74.77,
+        "eur" => 80,
+        "jpy" => 0.55,
+        "rub" => 1,
+        "cny" => 10.5
+    ];
+    public function getRates()
+    {
+        return $this->$rate;
+    }
+    public function getCurrencies()
+    {
+        return $this->$rate;
+    }
+    public function convert($from, $to, $amount)
+    {
+        return $this->$rate;
+    }
+}
